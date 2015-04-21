30d8fb6230012416cf3388a7e64e768d22179fa0
Matteo Menapace <hello@baddeo.com>
2015-04-21 18:32:00 +0100

    Comments on

--- demo-app/recipe-template.js
+++ demo-app/recipe-template.js
@@ -1,5 +1,11 @@
+// this function is like an HTML sausage machine
+// pass in some recipe data
+// and it will return an HTML list item wrapped around that data
 function getRecipeListItem(recipe)
 {
+	// create a variable to store the HTML code
+	// we put the static (non variable) bits in speech marks
+	// and the variable bits outside of speech marks
     var li = "<li>"
         + "<img src=" + recipe.image + ">"
         + "<h3>" + recipe.label + "</h3>"
