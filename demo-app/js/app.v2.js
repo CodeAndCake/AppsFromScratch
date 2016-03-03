ea919631b28fdb8accfeb8d71bbcbdd22644a657
Yuki Kumagai <yukik.dev@gmail.com>
2016-03-03 01:16:19 +0000

    simplifying code

---
+++ demo-app/js/app.v2.js
@@ -0,0 +1,78 @@
+
+// First let's store all the elements we need from Html
+
+var html = {
+	pageWrapper: $('article'),
+	pageSearch: $('#pageSearch'),
+	searchInput: $('#pageSearch input[type=search]'),
+	findButton: $('#pageSearch button'),
+	dropDown: $('#pageSearch select'),
+	results: $('#pageSearch .results'),
+	totalFound: $('#pageSearch .results .total'),
+	resultsList: $('#pageSearch .results ul'),
+	pageProfile: $('#pageProfile'),
+	backButton: $('#pageProfile .back'),
+	person: $('#pageProfile .person')
+};
+
+// Let's connect to the database
+
+var database = new Firebase('https://appsfromscratch.firebaseio.com/demo-app');
+
+// Here we define search functionality
+
+var pageSearchFunctionality = {
+	
+	onFindButtonClick: function(){
+		
+		// We should clear the previous search result if there is alreay
+		if(html.results.length > 0) html.results.empty();
+		
+		// Get the value from the dropdown 		
+ 		var selectedDropdownOption = html.dropDown.val();
+		
+		// Here is the way to get the data from the database
+ 		database.orderByChild("height").on("child_added", function(responce) {
+ 			
+ 			var data = responce.val();
+ 			var key = responce.key();
+
+   			personButton = $('<li id="' + key + '">' 
+	 			+ '<h2>' + data.name + '</h2>' 
+	 			+ '</li>');
+   			
+  			personButton.on('click', pageSearchFunctionality.onResultClick.bind(this, data));
+  			
+ 			html.results.append(personButton);
+ 			html.results.addClass('active');
+ 			
+  		});
+ 		
+ 	},
+ 	
+ 	onResultClick: function(data){
+   		
+ 		var profileHtml = $('<h2>' + data.name  + '</h2>' 
+	 		+ '<img src="' + data.image + '">' 
+	 		+ '<div class="about">' + data.about + '</div>'
+	 		+ '<button id="message">Message ' + data.name + ' now</button>');
+ 		
+ 		html.person.html(profileHtml);
+ 		
+ 		html.pageWrapper.addClass('profile');
+ 	},
+ 	
+ 	onBackButtonClick: function(){
+ 		html.pageWrapper.removeClass('profile');
+ 	},
+ 	
+};
+
+// Let's add initial behavior to the page elements
+
+html.findButton.on('click', pageSearchFunctionality.onFindButtonClick);
+html.backButton.on('click', pageSearchFunctionality.onBackButtonClick);
+ 
+
+ 
+  
